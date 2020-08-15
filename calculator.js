2c0570da26f24bcd3fb535301366524eaa9f8834
richiexuetang <49630208+richiexuetang@users.noreply.github.com>
2020-08-15 14:54:08 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("Thank you for using calculator, the result is " + result);
+});
+
+app.get("/bmicalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = Math.floor(weight / (height*height));
+    res.send("Thank you for using calculator, your BMI is " + bmi);
+});
+
+app.listen(3000);
